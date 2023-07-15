f2703f0b367c68adc5e58a7d13b1d5c21c43dff0
João Victorio <victoriojoao92@gmail.com>
2023-07-14 23:17:09 -0300

    primeiro commit

---
+++ ex01/script.js
@@ -0,0 +1,10 @@
+// Selecionar o elemento do header
+let header = document.querySelector('header');
+
+let logo = document.querySelector('.m-3');
+
+let menu = document.querySelectorAll('.nav-link');
+
+console.log(header);
+console.log(logo);
+console.log(menu);
