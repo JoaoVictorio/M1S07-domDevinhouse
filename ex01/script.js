d6ef23576b416594a3bd735c6121c476f712881b
João Victorio <victoriojoao92@gmail.com>
2023-07-16 02:12:43 -0300

    criando objeto menu

--- ex01/script.js
+++ ex01/script.js
@@ -3,9 +3,11 @@ let header = document.querySelector('#header');
 
 let logo = document.querySelector('.m-3');
 
+let = menu(){
+    
+}
 
-
-let menu = document.querySelectorAll('.nav-link');
+let menu = document.querySelectorAll('.nav-link'); 
 
 console.log(header);
 console.log(logo);
