96b47449c89e13f873885476989732613e6ad78a
João Victorio <victoriojoao92@gmail.com>
2023-07-16 21:07:21 -0300

    termina atv 2

--- ex01/script.js
+++ ex01/script.js
@@ -1,50 +1,41 @@
-// Selecionar o elemento do header
-// Objeto com as informações da logo e lista de opções de menu
 const headerInfo = {
   logoSrc: "/img/Dev +.png",
   logoAlt: "Logo da Farmácia Dev+",
-  opcoesMenu: [
+  menuOptions: [
     { label: "Medicamentos", link: "index.html" },
     { label: "Contato", link: "formbootstrap.html" },
     { label: "Sobre", link: "index.html" },
   ],
 };
 
-// Selecionar o elemento do header
-const headerElemento = document.getElementById("header");
+const headerElement = document.getElementById("header");
 
-// Criar o elemento da logo
-const logoElemento = document.createElement("img");
-logoElemento.classList.add("m-3");
-logoElemento.src = headerInfo.logoSrc;
-logoElemento.alt = headerInfo.logoAlt;
+const logoElement = document.createElement("img");
+logoElement.classList.add("m-3");
+logoElement.src = headerInfo.logoSrc;
+logoElement.alt = headerInfo.logoAlt;
 
-// Criar o elemento da navbar brand
-const brandElemento = document.createElement("a");
-brandElemento.classList.add("navbar-brand", "text-white", "d-flex");
-brandElemento.href = "#";
-brandElemento.textContent = "Farmácia Dev+";
+const brandElement = document.createElement("a");
+brandElement.classList.add("navbar-brand", "text-white", "d-flex");
+brandElement.href = "#";
+brandElement.textContent = "Farmácia Dev+";
 
-// Adicionar a logo e a navbar brand ao container do header
-headerElemento.querySelector(".container-fluid").prepend(logoElemento, brandElemento);
+headerElement.querySelector(".container-fluid").prepend(logoElement, brandElement);
 
-// Criar a lista de opções de menu
-const menuListElemento = document.createElement("ul");
-menuListElemento.classList.add("navbar-nav", "me-auto", "mb-2", "mb-lg-0");
+const menuListElement = document.createElement("ul");
+menuListElement.classList.add("navbar-nav", "me-auto", "mb-2", "mb-lg-0");
 
-// Adicionar as opções de menu à lista
-headerInfo.opcoesMenu.forEach((opcoes) => {
-  const menuItemElemento = document.createElement("li");
-  menuItemElemento.classList.add("nav-item");
+headerInfo.menuOptions.forEach((option) => {
+  const menuItemElement = document.createElement("li");
+  menuItemElement.classList.add("nav-item");
 
-  const menuLinkElemento = document.createElement("a");
-  menuLinkElemento.classList.add("nav-link", "active");
-  menuLinkElemento.href = opcoes.link;
-  menuLinkElemento.textContent = opcoes.label;
+  const menuLinkElement = document.createElement("a");
+  menuLinkElement.classList.add("nav-link", "active");
+  menuLinkElement.href = option.link;
+  menuLinkElement.textContent = option.label;
 
-  menuItemElemento.appendChild(menuLinkElemento);
-  menuListElemento.appendChild(menuItemElemento);
+  menuItemElement.appendChild(menuLinkElement);
+  menuListElement.appendChild(menuItemElement);
 });
 
-// Adicionar a lista de opções de menu ao header
-headerElement.querySelector(".navbar-collapse").appendChild(menuListElemento);
+headerElement.querySelector(".navbar-collapse").appendChild(menuListElement);
