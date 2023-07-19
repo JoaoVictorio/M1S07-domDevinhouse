97a7a1afb4a447bb63f92aaf73ad0512d82df0c6
João Victorio <victoriojoao92@gmail.com>
2023-07-19 16:58:17 -0300

    comeca ex-03

---
+++ ex3/script.js
@@ -0,0 +1,16 @@
+function mostrarInformacoesMedicamento(nomeMedicamento, listaMedicamentos) {
+  console.log(`Medicamento adicionado: ${nomeMedicamento}`);
+  console.log(`Lista de medicamentos: ${listaMedicamentos}`);
+}
+
+function adicionarMedicamentoAoCarrinho(event) {
+  event.preventDefault(); 
+
+  
+  const nomeMedicamento = event.target.parentElement.querySelector('.card-title').textContent;
+  
+  const listaMedicamentos = ['Concentril', 'Outro Medicamento', 'Mais um Medicamento'];
+
+  mostrarInformacoesMedicamento(nomeMedicamento, listaMedicamentos);
+  alert(`${nomeMedicamento} foi adicionado ao carrinho.`);
+}
